cf6f4b1c4ce01e08e5caa5816fe3ebf011b3901d
Ryan-Habets <507646@vistacollege.nl>
2023-10-11 19:26:17 +0200

    commit

---
+++ index1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>webshop</title>
+    <link rel="stylesheet" href="style1.css">
+</head>
+<body>
+    <div class='header'>
+        <h1>R&V</h1>
+    </div>
+
+
+    <div class='mid'>
+
+    </div>
+
+
+    <div class='footer'>
+</body>
+</html>
